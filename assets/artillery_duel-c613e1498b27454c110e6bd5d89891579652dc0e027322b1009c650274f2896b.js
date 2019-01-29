1b7777d27d53dac9a4e41dd5f032f57fa6277dce
Jason Ellis <palamedes@rocketmail.com>
2019-01-29 12:28:19 -0500

    update

---
+++ assets/artillery_duel-c613e1498b27454c110e6bd5d89891579652dc0e027322b1009c650274f2896b.js
@@ -0,0 +1,10 @@
+// Testing around my artillery duel sim 
+
+
+$(document).on('ready turbolinks:load', function() {
+  var $adCanvas = $('#artilleryduel');
+
+
+  console.log('here');
+  console.log($adCanvas);
+});
